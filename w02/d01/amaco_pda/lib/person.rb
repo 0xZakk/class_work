0f9002c8b3b5b09b6c12601f070f512d8bc5ba22
ZAKK <zachary.fleischmann@gmail.com>
2015-03-23 15:10:24 -0400

    define person class and switch out personal_info has for person class/methods

---
+++ w02/d01/amaco_pda/lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+	attr_accessor :name, :age, :occupation, :favorite_food
+
+	def initialize(name, age, occupation, favorite_food)
+		@name = name
+		@age = age
+		@occupation = occupation
+		@favorite_food = favorite_food
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
